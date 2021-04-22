1a87227f770e4651a8757f9d0ed7bba10a125caf
ruiliangwu@yangqianguan.com <ruiliangwu@yangqianguan.com>
2021-04-22 20:17:01 +0800

    jmh基准测试项目

---
+++ src/main/java/com/deepoove/MyBenchmark.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.deepoove;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.Throughput)
+@Measurement(iterations = 2, time = 6, timeUnit = TimeUnit.SECONDS)
+@Threads(4)
+@Fork(2)
+@Warmup(iterations = 1)
+@State(value = Scope.Benchmark)
+public class MyBenchmark {
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+            .include(MyBenchmark.class.getSimpleName())
+            .build();
+        new Runner(opt).run();
+    }
+
+    @Param(value = { "10", "50", "100", "1000" })
+    private int length;
+
+    @Benchmark
+    public void testStringAdd(Blackhole blackhole) {
+        String a = "";
+        for (int i = 0; i < length; i++) {
+            a += i;
+        }
+        blackhole.consume(a);
+    }
+
+    @Benchmark
+    public void testStringBuilderAdd(Blackhole blackhole) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(i);
+        }
+        blackhole.consume(sb.toString());
+    }
+
+}
